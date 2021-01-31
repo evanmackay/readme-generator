afd50b26a511504c2f14e25816c36dbb2d7621be
Evan Mackay <evanmackay@MacBook-Pro.local>
2021-01-31 13:38:37 -0600

    rewrote code for this assignment

--- index.js
+++ index.js
@@ -1,156 +1,110 @@
-const axios = require("axios");
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require("fs");
-const template = require('./template.js');
-const validator = require('email-validator');
+const util = require("util");
 
-let gitHubUserData;
-let gitHubRepos;
-let selectedRepo;
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// Verify that a given GitHub exists and has at least one repo
-async function verifyGitHubAccount(username) {
 
-    if(username.length === 0) 
-        return 'username cannot be blank';
-    else{
-
-        const url = `https://api.github.com/users/${username}`;
-
-        // User GitHubUser Data
-        const userResp = await axios.get(url)
-
-        gitHubUserData = userResp.data;
-
-        reposUrl = userResp.data.repos_url;
-
-        if(typeof reposUrl === 'undefined')
-            return `GitHub username ${username} does not exist`;
-
-        reposResp = await axios.get(reposUrl);
-
-        const reposData = reposResp.data;
-
-        if(reposData.length === 0)
-            return `GitHub user ${username} has no repos`;
-
-        gitHubRepos = reposData;
-        
-        questions[1].choices = reposData.map(repo => repo.name);
-
-        return true;
-    }
-}   
-
-// Filter callback for repo selection question. Sets defaults for subsequent
-// questions based on selected repo
-function setRepoDefaults(repoName) {
-
-    return new Promise((resolve,reject) => {
-
-        selectedRepo = gitHubRepos.find(repo => repo.name == repoName);
-
-        // Set repo name and description as defaults for Title and Description 
-        // question
-        questions[2].default = selectedRepo.description;
-
-        // Get contributors and tags from repo
-        axios
-        .all([
-            axios.get(selectedRepo.contributors_url),
-            axios.get(selectedRepo.tags_url)
-        ])
-        .then(respArr => {
-            // Set repo contributors as default for Contributors question
-            questions[7].default = respArr[0].data.map(contributor => contributor.login).join(',');
-
-            resolve(repoName);
-        })
-        .catch(err => {
-            reject(new Error("Could not set defaults"));
-        });
-    })
+let promptUser = () => {
+    
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the title of your project?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Please provide a description of the project.",
+                name: "description"
+            },
+            {
+                type: "checkbox",
+                message: "Select what you would like to be included in your table of contents.",
+                choices: ["Installation", "Usage", "Credits"],
+                name: "content"
+            },
+            {
+                type: "input",
+                message: "Provide installation instructions for how to get your project working on your local machine.",
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: "How will this project be used?",
+                name: "usage"
+            },
+            {
+                type: "list",
+                message: "Select the License you would like to use.",
+                choices: ["Apache License v2 ![Apache License](https://img.shields.io/badge/license-Apache-blue)", "GNU General Public License v3 ![GNU General Public License](https://shields.io/badge/license-GPL%20(%3E%3D%202)-blue)", "MIT License ![MIT License](https://img.shields.io/badge/license-MIT-green)"],
+                name: "license"
+            },
+            {
+                type: "input",
+                message: "Who contributed to this project?",
+                name: "contributers"
+            },
+            {
+                type: "input",
+                message: "Write any test you would like to be performed on your application here",
+                name: "tests"
+            },
+            {
+                type: "input",
+                message: "What are some questions you would like to pose to the user?",
+                name: "questions"
+            }
+        ]);
 }
 
-function validateEmail(email) {
-  
-    if(validator.validate(email))
-        return true;
+let generateREADME = (answers) => {
 
-    return `${email} is not a valid email`;
-}
-
-const questions = [
-    {
-        name: "username",
-        message: "What is your GitHub username?",
-        default: "evanmackay",
-        validate: verifyGitHubAccount
-    },
-    {
-        type: "list",
-        name: "repoName",
-        message: "Select the project repo:",
-        filter:setRepoDefaults
-    },
-    {
-        name: "title",
-        message: "Enter a project title:",
-    },
-    {
-        name: "description",
-        message: "Enter a project description:",
-    },
-    {
-        name: "installation",
-        message: "Enter installation instructions:"
-    },
-    {
-        name: "usage",
-        message: "Enter usage directions:"
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Select license type:",
-        choices: ["copyleft","lpgl","MIT","permissive","proprietary","public"]
-    },
-    {
-        name: "contributors",
-        message: "Enter contibutors:"
-    },
-    {
-        name: "tests",
-        message: "Enter tests:"
-    },
-    {
-        name: "email",
-        message: "Enter contact email:",
-        validate: validateEmail
-    }
-];
-
-function init() {
-
-    inquirer.prompt(questions).then(resp => {
-
-        generateReadMe(resp);
-    });
-}
-
-function generateReadMe(responses){
-
-    fs.writeFile
-    (
-        "./output/README.md",
-        template.getReadMe(gitHubUserData,responses),
-        (err) => {
-            if(err)
-                console.log("An error occured while writing file");
-            else
-                console.log("File saved");
+    let returnContent = () => {
+        for (let i = 0; i < answers.content.length; i++) {
+            return `* [${answers.content[i]}](#${answers.content[i].toLowerCase()})`
         }
-    );
+    }
+    return `# ${answers.title}
+
+${answers.license}
+
+## Project Description
+${answers.description}
+
+## Table of Contents
+${returnContent()}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributers
+${answers.contributers}
+
+## Tests
+${answers.tests}
+
+## Questions
+${answers.questions}`;
 }
 
-init();
+async function init() {
+    console.log("test")
+    try {
+        const answers = await promptUser();
+
+        const markDown = generateREADME(answers);
+
+        await writeFileAsync("README.md", markDown);
+
+        console.log("Successfully wrote to README.md");
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+init()
